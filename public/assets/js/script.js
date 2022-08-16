55f21fb458455ef943e8e9cb0ed1f11cbc548891
Taco-Edge <anthonin.mouveaux@outlook.fr>
2022-08-16 13:49:21 +0200

    Amélioration de l'affichage

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,6 +4,8 @@ const compWeaponText = document.querySelector('#compWeapon p');
 const playerWeaponText = document.querySelector('#playerWeapon p');
 const compModel = document.querySelector('.compModel');
 const playerModel = document.querySelector('.playerModel');
+let scorePlayer = 0;
+let scoreComp = 0;
 
 function fight(weapon) {
 
@@ -12,7 +14,7 @@ function fight(weapon) {
     let compPick = Math.floor(Math.random() * 3);
     if (compPick == 0) {
         compSwordPick.textContent++;
-        compWeaponText.textContent = 'Epée';
+        compWeaponText.textContent = 'Epee';
     } else if (compPick == 1) {
         compLancePick.textContent++;
         compWeaponText.textContent = 'Lance';
@@ -23,10 +25,10 @@ function fight(weapon) {
 
     //INCREMENTATION DE LA PICKRATE DU JOUEUR
 
-    if (weapon == 'Epée') {
+    if (weapon == 'Epee') {
         playerSwordPick.textContent++;
-        playerWeaponText.textContent = 'Epée';
-    } else if (weapon == "Lance") {
+        playerWeaponText.textContent = 'Epee';
+    } else if (weapon == 'Lance') {
         playerLancePick.textContent++;
         playerWeaponText.textContent = 'Lance';
     } else {
@@ -34,97 +36,141 @@ function fight(weapon) {
         playerWeaponText.textContent = 'Hache';
     }
 
-    //SHIFUMI DES ARMES
+    //SCORE ET PV
 
-    if ((weapon == 'Epée' && compPick == 2) || (weapon == 'Lance' && compPick == 0) || (weapon == 'Hache' && compPick == 1)) {
-        result.textContent = 'Gagné';
+    if ((weapon == 'Epee' && compPick == 2) || (weapon == 'Lance' && compPick == 0) || (weapon == 'Hache' && compPick == 1)) {
+        result.textContent = 'Gagne';
         if (compBarInner.classList.contains('firstLoss')) {
-            compBarInner.classList.toggle('firstLoss');
-            compBarInner.classList.toggle('secondLoss');
-            compLifeAmount.textContent = '10';
+            setTimeout(function () {
+                compBarInner.classList.toggle('firstLoss');
+                compBarInner.classList.toggle('secondLoss');
+                compLifeAmount.textContent = '10';
+            }, 1100)
         } else if (compBarInner.classList.contains('secondLoss')) {
-            compBarInner.classList.toggle('secondLoss');
-            compBarInner.classList.toggle('lastLoss');
-            compLifeAmount.textContent = '0';
+            setTimeout(function () {
+                compBarInner.classList.toggle('secondLoss');
+                compBarInner.classList.toggle('lastLoss');
+                compLifeAmount.textContent = '0';
+            }, 1100)
         } else {
-            compBarInner.classList.toggle('firstLoss');
-            compLifeAmount.textContent = '20';
+            setTimeout(function () {
+                compBarInner.classList.toggle('firstLoss');
+                compLifeAmount.textContent = '20';
+            }, 1100)
         }
-    } else if ((weapon == 'Epée' && compPick == 1) || (weapon == 'Lance' && compPick == 2) || (weapon == 'Hache' && compPick == 0)) {
+        scorePlayer++;
+    } else if ((weapon == 'Epee' && compPick == 1) || (weapon == 'Lance' && compPick == 2) || (weapon == 'Hache' && compPick == 0)) {
         result.textContent = 'Perdu';
         if (playerBarInner.classList.contains('firstLoss')) {
-            playerBarInner.classList.toggle('firstLoss');
-            playerBarInner.classList.toggle('secondLoss');
-            playerLifeAmount.textContent = '10';
+            setTimeout(function () {
+                playerBarInner.classList.toggle('firstLoss');
+                playerBarInner.classList.toggle('secondLoss');
+                playerLifeAmount.textContent = '10';
+            }, 1100)
         } else if (playerBarInner.classList.contains('secondLoss')) {
-            playerBarInner.classList.toggle('secondLoss');
-            playerBarInner.classList.toggle('lastLoss');
-            playerLifeAmount.textContent = '0';
+            setTimeout(function () {
+                playerBarInner.classList.toggle('secondLoss');
+                playerBarInner.classList.toggle('lastLoss');
+                playerLifeAmount.textContent = '0';
+            }, 1100)
         } else {
-            playerBarInner.classList.toggle('firstLoss');
-            playerLifeAmount.textContent = '20';
+            setTimeout(function () {
+                playerBarInner.classList.toggle('firstLoss');
+                playerLifeAmount.textContent = '20';
+            }, 1100)
         }
+        scoreComp++
     } else {
-        result.textContent = 'Egalité'
+        result.textContent = 'Egalite'
     }
+
+    //CALCUL DU SCORE
+
+    // if (scorePlayer == 0 && scoreComp == 0) {
+
+	// } else {
+	// 	ratio.innerHTML = Math.round(scorePlayer / (scorePlayer + scoreComp) * 100) + '%';
+	// }
 
     //ANIMATION DES PERSONNAGES
 
-    if (result.textContent == 'Gagné') {
-        if (weapon == 'Epée') {
+    if (result.textContent == 'Gagne') {
+        if (weapon == 'Epee') {
             playerModel.className = 'playerModel';
             compModel.className = 'compModel';
             playerModel.classList.add('swordWin');
             compModel.classList.add('axe');
+            setTimeout(function () { playerModel.className = 'playerModel sword'; }, 3500);
         } else if (weapon == 'Lance') {
             playerModel.className = 'playerModel';
             compModel.className = 'compModel';
             playerModel.classList.add('lanceWin');
             compModel.classList.add('sword');
+            setTimeout(function () { playerModel.className = 'playerModel lance'; }, 3500);
         } else {
             playerModel.className = 'playerModel';
             compModel.className = 'compModel';
             playerModel.classList.add('axeWin');
             compModel.classList.add('lance');
+            setTimeout(function () { playerModel.className = 'playerModel axe'; }, 3500);
         }
     } else if (result.textContent == 'Perdu') {
-        if (weapon == 'Epée') {
+        if (compPick == 0) {
             playerModel.className = 'playerModel';
             compModel.className = 'compModel';
             compModel.classList.add('swordWin');
             playerModel.classList.add('axe');
-        } else if (weapon == 'Lance') {
+            setTimeout(function () { compModel.className = 'compModel sword'; }, 3500);
+        } else if (compPick == 1) {
             playerModel.className = 'playerModel';
             compModel.className = 'compModel';
             compModel.classList.add('lanceWin');
             playerModel.classList.add('sword');
+            setTimeout(function () { compModel.className = 'compModel lance'; }, 3500);
         } else {
             playerModel.className = 'playerModel';
             compModel.className = 'compModel';
             compModel.classList.add('axeWin');
             playerModel.classList.add('lance')
+            setTimeout(function () { compModel.className = 'compModel axe'; }, 3500);
         }
     } else {
-        if (weapon == 'Epée') {
+        if (weapon == 'Epee') {
             playerModel.className = 'playerModel';
             compModel.className = 'compModel';
-            compModel.classList.add('sword');
-            playerModel.classList.add('sword');
+            compModel.classList.add('swordWin');
+            playerModel.classList.add('swordWin');
+            setTimeout(function () {
+                playerModel.className = 'playerModel sword';
+                compModel.className = 'compModel sword';
+            }, 3500)
         } else if (weapon == 'Lance') {
             playerModel.className = 'playerModel';
             compModel.className = 'compModel';
-            compModel.classList.add('lance');
-            playerModel.classList.add('lance');
+            compModel.classList.add('lanceWin');
+            playerModel.classList.add('lanceWin');
+            setTimeout(function () {
+                playerModel.className = 'playerModel lance';
+                compModel.className = 'compModel lance';
+            }, 3500)
         } else {
             playerModel.className = 'playerModel';
             compModel.className = 'compModel';
-            compModel.classList.add('axe');
-            playerModel.classList.add('axe')
+            compModel.classList.add('axeWin');
+            playerModel.classList.add('axeWin')
+            setTimeout(function () {
+                playerModel.className = 'playerModel axe';
+                compModel.className = 'compModel axe';
+            }, 3500)
         }
     }
 
-    console.log(weapon, compPick);
-
+    // if (scorePlayer === 3) {
+    //     gameEnd.style.display = 'block';
+    // }
+    // if (scoreComp === 3) {
+    //     gameEnd.style.display = 'block';
+    // }
 }
 
 
